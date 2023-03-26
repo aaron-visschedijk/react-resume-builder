66c1d9e8460bbcf66ede64147afa5fa52b5174ef
aaron-visschedijk <aaron.visschedijk@gmail.com>
2023-03-26 11:16:21 +0200

    Use all components to create a resume

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import About from './components/About';
+import Education from './components/Education';
+import Experience from './components/Experience';
+import Languages from './components/Languages';
+import Contact from './components/Contact';
+import Publication from './components/Publication';
 
-function App() {
+import { ResumeData } from './dataModel';
+
+
+function App(profile: ResumeData) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="sidebar">
+        <div className='centered'>
+          <img className="photo" alt='profile' src={profile.photo} />
+        </div>
+
+        <Contact
+          {...profile.contact}
+        />
+        <hr className="divider" />
+        <About
+          {...profile.about}
+        />
+        <hr className="divider" />
+        <Languages
+          {...profile.languages}
+        />
+      </div>
+      <div className="main">
+        <h1 className="name">{profile.name}</h1>
+        <h2 className='title'>{profile.title}</h2>
+        <hr className="divider" />
+        <Experience
+          {...profile.experiences}
+        />
+        <hr className="divider" />
+        <Education
+          {...profile.education}
+        />
+        <hr className="divider" />
+        <Publication
+          {...profile.publications}
+        />
+      </div>
     </div>
   );
 }
