fdbecd2a304e91064aa9df9dbd1b1cf35e18e66d
aaron-visschedijk <aaron.visschedijk@gmail.com>
2023-03-26 11:17:04 +0200

    use data instance to populate resume

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { profile } from './data';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<App {...profile} />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { profile } from './data';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      {...profile}
+    />
   </React.StrictMode>
 );
 
